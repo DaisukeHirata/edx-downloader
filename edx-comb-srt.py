1da3c41c1a6eb1e6590467ff899159a8fcca1f33
Daisuke Hirata <daisukihirata@gmail.com>
2013-09-20 21:36:13 +0900

    initial commit

---
+++ edx-comb-srt.py
@@ -0,0 +1,31 @@
+import os
+import glob
+import commands
+
+DEFAULT_DOWNLOAD_DIRECTORY = './Downloaded'
+
+if __name__ == '__main__':
+
+    courses = os.listdir(DEFAULT_DOWNLOAD_DIRECTORY)
+
+    for i, course in enumerate(courses):
+        print(str(i+1) + ' : ' + course)
+
+    c_number = int(input('Enter Course Number: '))
+
+    course_dir = DEFAULT_DOWNLOAD_DIRECTORY + '/' + courses[c_number-1]
+
+    mp4_list = glob.glob(course_dir + '/*.mp4')
+
+    for mp4 in mp4_list:
+        filename, ext = os.path.splitext(mp4)
+        srt = filename + '.en.srt'
+        outfile = course_dir + '/srt_' + os.path.basename(mp4)
+        cmd = 'MP4Box -add "%s" -add "%s":hdlr=sbtl:lang=eng -new "%s"' % (mp4, srt, outfile)
+        status, cmdoutput = commands.getstatusoutput(cmd)
+        if status == 0:
+            print(outfile)
+        else:
+            print(cmdoutput)
+
+
